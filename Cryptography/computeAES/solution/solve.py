8ff571f85089e12305b424d00c1755bd095eff05
ZivtinHD <43054907+ZivtinHD@users.noreply.github.com>
2018-09-06 22:36:19 -0400

    Needed update

--- Cryptography/computeAES/solution/solve.py
+++ Cryptography/computeAES/solution/solve.py
@@ -1,6 +1,6 @@
 from Crypto.Cipher import AES
 
-cipher = AES.new('6v3TyEgjUcQRnWuIhjdTBA=='.decode('base64'),AES.MODE_ECB)
-decoded = cipher.decrypt('rW4q3swEuIOEy8RTIp/DCMdNPtdYopSRXKSLYnX9NQe8z+LMsZ6Mx/x8pwGwofdZ'.decode('base64'))
+cipher = AES.new('azdvtH4bvfdS/mryKLTNqQ=='.decode('base64'),AES.MODE_ECB)
+decoded = cipher.decrypt('R9TacKHy6cf1AZho/nwWWYaNzP5GfltKE5yW+kwRYe0LY+PdGk1hfoanS/iVZ7z1'.decode('base64'))
 
 print decoded
